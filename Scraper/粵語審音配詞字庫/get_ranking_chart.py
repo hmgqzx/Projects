c0e292a87f57fbb7326d88d41db4999a73daac4c
hmgqzx <hmgqzx@gmail.com>
2019-04-08 00:06:54 +0800

    词条具体信息爬取

--- Scraper/粵語審音配詞字庫/get_ranking_chart.py
+++ Scraper/粵語審音配詞字庫/get_ranking_chart.py
@@ -1,13 +1,13 @@
 import requests
 import csv
 from bs4 import BeautifulSoup
-
+import re
 
 def get(s):
     r = requests.get('http://humanum.arts.cuhk.edu.hk/Lexis/lexi-can/faq.php?s='+s)
     r.encoding='big5'
     html=r.text
-
+    # print(html)
     bssoup = BeautifulSoup(html,"html.parser")
     table = bssoup.find("table")
     items = table.find_all("td")
@@ -16,12 +16,15 @@ def get(s):
         rank=items[i].string
         freq=items[i+1].string
         chrct=items[i+2].string
-        row=(rank,freq,chrct)
+        href=items[i+2].a.get('href')
+        code=re.search('%.*',href).group()
+        row=(rank,freq,chrct,code)
+        # print(row)
         with open('RankingChart.csv','a') as f:
             f_csv = csv.writer(f)
             f_csv.writerow(row)
 
-headers = ['Ranking','Freq.','Characters']
+headers = ['Ranking','Freq.','Characters','Code']
 with open('RankingChart.csv','w') as f:
     f_csv = csv.writer(f)
     f_csv.writerow(headers)
