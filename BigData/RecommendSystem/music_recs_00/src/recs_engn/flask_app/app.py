bec780998082219b85efe16322bee1deec64b287
hmgqzx <hmgqzx@gmail.com>
2019-04-11 18:58:06 +0800

    resolve parameters of the request

--- BigData/RecommendSystem/music_recs_00/src/recs_engn/flask_app/app.py
+++ BigData/RecommendSystem/music_recs_00/src/recs_engn/flask_app/app.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'hello, Flask'
+    # resolve parameters of the request:
+    # http://127.0.0.1:5050/?itemId=aba32456oeutpraoneucc&userId=ao1235123ec&type=old&ip=127.0.0.1
+    item_id = request.args.get('itemId', '')
+    user_id = request.args.get('userId', '')
+    action_type = request.args.get('type', '')
+    ip = request.args.get('ip', '')
+    string = item_id + ' ' + user_id + ' ' + action_type + ' ' + ip
+    return string
 
 
 app.run(host='127.0.0.1', port='5050', debug=True)
