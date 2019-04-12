6da9b94ac7839e3c67dea482de6acfa77a488437
hmgqzx <hmgqzx@gmail.com>
2019-04-12 15:06:38 +0800

    decode input file's line

--- BigData/RecommendSystem/music_recs_00/src/data_preprcss/pre_data_for_cb/gen_cb_train.py
+++ BigData/RecommendSystem/music_recs_00/src/data_preprcss/pre_data_for_cb/gen_cb_train.py
@@ -3,3 +3,10 @@ out_file = '../../../data/cb_train.data'
 
 with open(out_file, 'w') as f:
     pass
+
+with open(in_file, 'r') as f_in:
+    for line in f_in:
+        ss = line.strip().split('\001')
+        (user_id, item_id, listen_len, listen_moment,
+         gender, age, salary, user_loc,
+         name, desc, total_time, item_loc, tags) = ss
