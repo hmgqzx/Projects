bdefe12575ded6c72a20ca4dc1e34639a53cf810
hmgqzx <hmgqzx@gmail.com>
2019-04-12 16:17:28 +0800

    handle `tags` with idf.txt

--- BigData/RecommendSystem/music_recs_00/src/data_preprcss/pre_data_for_cb/gen_cb_train.py
+++ BigData/RecommendSystem/music_recs_00/src/data_preprcss/pre_data_for_cb/gen_cb_train.py
@@ -6,8 +6,17 @@ out_file = '../../../data/cb_train.data'
 with open(out_file, 'w') as f:
     pass
 
+idf_file = '../../../data/idf.txt'
+idf_dict = {}
+with open(idf_file, 'r') as f:
+    for line in f:
+        word, idf_score = line.strip().split(' ')
+        idf_dict[word] = idf_score
+
+# weight of fields
 RADIO_FOR_NAME = 0.9
 RADIO_FOR_DESC = 0.1
+RADIO_FOR_TAGS = 0.05
 
 with open(in_file, 'r') as f_in:
     for line in f_in:
@@ -29,6 +38,13 @@ with open(in_file, 'r') as f_in:
                 keyword_dict[keyword] += score * RADIO_FOR_DESC
             else:
                 keyword_dict[keyword] = score * RADIO_FOR_DESC
+        # handle `tags` with idf.txt
+        for tag in tags.strip().split(','):
+            if tag not in idf_dict:
+                continue
+            else:
+                (keyword, score) = (tag, float(idf_dict[tag]))
+                keyword_dict[keyword] = score * RADIO_FOR_TAGS
 
         # print(item_id)
         # print(keyword_dict)
