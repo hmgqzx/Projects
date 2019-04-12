63337fb8a65913f0a1e9849dbe6fc6978efffecb
hmgqzx <hmgqzx@gmail.com>
2019-04-12 19:40:54 +0800

    reformat

--- BigData/RecommendSystem/music_recs_00/src/data_preprcss/pre_base_data/gen_base.py
+++ BigData/RecommendSystem/music_recs_00/src/data_preprcss/pre_base_data/gen_base.py
@@ -1,5 +1,3 @@
-import sys
-
 # TODO: import path by config will be better
 user_action_data = '../../../data/user_watch_pref.sml'
 music_meta_data = '../../../data/music_meta'
@@ -8,12 +6,12 @@ user_profile_data = '../../../data/user_profile.data'
 merged_data = '../../../data/merged_base.data'
 
 # touch new file: bcz "with open(output_data,'a') as f" below
-with open(merged_data,'w') as fa:
+with open(merged_data, 'w') as fa:
     pass
 
 # 1. decode music meta data
-item_info_dict = {} # key:item_id, value: name, desc, total_time, loc, tags
-with open(music_meta_data,'r') as f:
+item_info_dict = {}  # key:item_id, value: name, desc, total_time, loc, tags
+with open(music_meta_data, 'r') as f:
     for line in f:
         ss = line.strip().split('\001')
         if len(ss) != 6:
@@ -24,8 +22,8 @@ with open(music_meta_data,'r') as f:
     # print(item_info_dict)
 
 # 2. decode user profile data
-user_profile_dict = {} # key:user_id, value: user_id, gender, age, salary, loc
-with open(user_profile_data,'r') as f:
+user_profile_dict = {}  # key:user_id, value: user_id, gender, age, salary, loc
+with open(user_profile_data, 'r') as f:
     for line in f:
         ss = line.strip().split(',')
         if len(ss) != 5:
@@ -40,13 +38,13 @@ with open(user_profile_data,'r') as f:
 
 
 # 3. decode user action dat
-with open(user_action_data,'r') as f:
+with open(user_action_data, 'r') as f:
     for line in f:
         ss = line.strip().split('\001')
         # print(ss)
         if len(ss) != 4:
             continue
-        user_id,item_id, listen_len, listen_moment = ss
+        user_id, item_id, listen_len, listen_moment = ss
         # filter
         if user_id not in user_profile_dict:
             continue
@@ -54,8 +52,8 @@ with open(user_action_data,'r') as f:
             continue
         # join 3 tables
         record = '\001'.join([user_id, item_id, listen_len, listen_moment,
-                         user_profile_dict[user_id],
-                         item_info_dict[item_id]])
-        with open(merged_data,'a') as f_op:
+                              user_profile_dict[user_id],
+                              item_info_dict[item_id]])
+        with open(merged_data, 'a') as f_op:
             f_op.write(record)
             f_op.write('\n')
