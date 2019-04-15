35a455fe44afaea055c3473feb4190e61922f37b
hmgqzx <hmgqzx@gmail.com>
2019-04-15 18:52:41 +0800

    分割释义
    先根据词性分割释义，再对每个词性下的释义根据序号分割，返回结果列表

--- Scraper/文言文高频字库/src/match.py
+++ Scraper/文言文高频字库/src/match.py
@@ -13,3 +13,42 @@ fake_meaning_text = "[代]\
 唯，表應答。如：「斐然」、「赫然」、「恍然」。唐．柳宗元〈桐葉封弟辯〉：「周公曰：『天子不可戲。』乃封小弱弟於唐。吾意不然。」\
 [名]\
 姓。如漢代有然溫。"
+
+
+def split_meaning_text(meaning_text):
+    """根據詞性對詞條釋義文本進行分割
+    :param meaning_text: 詞條釋義文本
+    :return pos_meaning_list: 由各個釋義所組成的列表"""
+    pos_meaning_list = re.findall(r'\[.][^\[]*', meaning_text)
+    print("pos_meaning_list:")
+    print(pos_meaning_list)
+    return pos_meaning_list
+
+
+def extract_pos(pos_meaning):
+    pos = re.match(r'\[\w]', pos_meaning).group()
+    print('pos:')
+    print(pos)
+    return pos
+
+
+def split_pos_meaning(pos_meaning):
+    meanings = re.findall(r'\d[^\d]*', pos_meaning)
+    if meanings:
+        print('meanings:')
+        print(meanings)
+        return meanings
+    else:
+        meaning = re.sub(r'\[.]', '', pos_meaning)
+        print('meaning:')
+        print([meaning])
+        return [meaning]
+
+
+if __name__ == '__main__':
+    pos_meaning_list = split_meaning_text(fake_meaning_text)
+    for pos_meaning in pos_meaning_list:
+        pos = extract_pos(pos_meaning)
+        meaning_list = split_pos_meaning(pos_meaning)
+        for meaning in meaning_list:
+            pass
