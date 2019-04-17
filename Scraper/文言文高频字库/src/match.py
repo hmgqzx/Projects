dc89b1e8ff8fd0fc6af8f39b803a01d49e365394
hmgqzx <hmgqzx@gmail.com>
2019-04-17 23:37:55 +0800

    ADD func of extract `index`, `gloss` and `quote` of a `meaning`

--- Scraper/文言文高频字库/src/match.py
+++ Scraper/文言文高频字库/src/match.py
@@ -29,6 +29,43 @@ def split_pos_meaning(pos_meaning):
         return [meaning]
 
 
+def extract_index(meaning):
+    index = '0'
+    m = re.match(r'\d', meaning)
+    if m:
+        index = m.group()
+    return index
+
+
+def extract_gloss(meaning):
+    gloss = ''
+    m = re.match(r'(?:\d\.)?([^《》〈〉]*。)(?:(?:\w．)|(?:《.*》：))?', meaning)
+    if m:
+        gloss = m.group(1)
+    return gloss
+
+
+def extract_quote(meaning):
+    quote = []
+    book_quote = re.findall(r'(?:\w．)?\w{0,5}《[^》(?:說文解字)(?:廣韻)(?:玉篇)(?:爾雅)]+》：「[^《〈]*?」', meaning)
+    article_quote = re.findall(r'(?:\w．)?\w{0,5}〈[^〉]+〉\w{0,3}：「[^《〈]*?」', meaning)
+    if book_quote:
+        quote += book_quote
+    if article_quote:
+        quote += article_quote
+    if len(quote) == 4:
+        pass
+    elif len(quote) == 3:
+        quote += ['']
+    elif len(quote) == 2:
+        quote += ['', '']
+    elif len(quote) == 1:
+        quote += ['', '', '']
+    else:
+        quote = ['', '', '', '']
+    return quote
+
+
 if __name__ == '__main__':
     pos_meaning_list = split_meaning_text(fake_meaning_text)
     for pos_meaning in pos_meaning_list:
